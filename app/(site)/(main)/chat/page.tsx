1058d0d7264bbe0b95c063151fddac33ea9c0027
Abdul Rahmat <abdulrahmat97dev@gmail.com>
2023-11-30 14:07:03 +0700

    add auth ui

---
+++ app/(site)/(main)/chat/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function ChatPage() {
+  return (
+    <div className="hidden lg:block lg-pl-80 h-full">
+        <EmptyState/>
+    </div>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  darkMode: 'class',
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -8,13 +9,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
     },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: 'class', // only generate classes
+    }),
+  ],
 }
 export default config
