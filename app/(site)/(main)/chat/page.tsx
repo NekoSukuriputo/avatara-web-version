5a68fb8a5d4771eaeee4260f0a972d20ca2b92d7
Abdul Rahmat <abdulrahmat97dev@gmail.com>
2023-11-30 17:26:27 +0700

    add chat ui

--- app/(site)/(main)/chat/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-export default function ChatPage() {
-  return (
-    <div className="hidden lg:block lg-pl-80 h-full">
-        <EmptyState/>
-    </div>
-  )
-}
---
+++ app/(site)/users/page.tsx
@@ -0,0 +1,10 @@
+
+import EmptyState from "@/app/components/EmptyState";
+
+export default function ChatPage() {
+  return (
+    <div className="hidden lg:block lg:pl-80 h-full">
+      <EmptyState />
+    </div>
+  );
+}
